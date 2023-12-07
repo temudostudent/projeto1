d9dd58a8dfb703150b9691bb6f563b3f851840c4
temudostudent <cesarneves@student.dei.uc.pt>
2023-12-07 21:38:06 +0000

    nova classe

--- ProjetoRockStar/src/Musica.java
+++ ProjetoRockStar/src/Musica.java
@@ -2,12 +2,13 @@ import java.io.Serializable;
 import java.time.LocalDate;
 import java.time.Year;
 import java.util.ArrayList;
+import java.util.Date;
 
 public class Musica implements Serializable {
     //Atributos
     protected String titulo;
     protected Artista artista;
-    protected LocalDate ano;
+    protected Date ano;
     protected double duracao;
     protected String genero;
     protected ArrayList<Integer> rating;
@@ -15,10 +16,10 @@ public class Musica implements Serializable {
     protected boolean estado;
 
     //Construtor que recebe titulo,ano,duracao,genero,estado
-    public Musica(String titulo, Artista artista, LocalDate ano, double duracao, String genero, boolean estado) {
+    public Musica(String titulo, Artista artista, int ano, double duracao, String genero, boolean estado) {
         this.titulo = titulo;
         this.artista = artista;
-        this.ano = ano;
+        this.ano = new Date(ano);
         this.duracao = duracao;
         this.genero = genero;
         this.estado = estado;
