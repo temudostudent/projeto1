6bd27eed9b9017fb76dbc2e0c645d65a22eaea8b
temudostudent <cesarneves@student.dei.uc.pt>
2023-12-07 21:34:07 +0000

    nova classe

--- ProjetoRockStar/src/Artista.java
+++ ProjetoRockStar/src/Artista.java
@@ -5,13 +5,23 @@ public class Artista extends Utilizador implements Serializable {
 
     //Atributos
     private int pin;
-    private ArrayList<Album> albuns;
+    private ArrayList<PlayList> albuns;
     private ArrayList<Musica> musicas;
 
     //Construtor que recebe username, password, pin
-
     public Artista(String username, String password, int pin) {
         super(username, password);
         this.pin = pin;
+    }
+
+    //Imprimir todas as músicas do artista
+    @Override
+    public void verListas() {
+        musicas.forEach(System.out::println);
+    }
+
+    @Override
+    public void criarListaMusicas(String nomeDoAlbum,boolean visibilidade) {
+        PlayList nova=new PlayList(nomeDoAlbum,visibilidade);
     }
 }
