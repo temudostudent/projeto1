3619226fa347c81bb4c1815da8bad1125f170081
temudostudent <cesarneves@student.dei.uc.pt>
2023-12-13 10:27:48 +0000

    nova classe

--- ProjetoRockStar/src/PlayList.java
+++ ProjetoRockStar/src/PlayList.java
@@ -1,24 +1,46 @@
 import java.util.ArrayList;
+import java.util.Random;
 
-public class PlayList {
-    private String nome;
+public class PlayList extends ConjuntoMusicas{
+
    private  boolean visibilidade;
 
-   private ArrayList<Musica> musicas;
-
     public PlayList(String nome, boolean visibilidade) {
-        this.nome = nome;
+        super(nome);
         this.visibilidade = visibilidade;
+        super.musicas=new ArrayList<>();
     }
 
-    //Adiciona música
-    public  void addMusica(Musica m){
-        musicas.add(m);
+    @Override
+    public void adicionarMusica(Musica m) {
+        this.musicas.add(m);
     }
 
-    //Remove música
-    public  void removeMusica(Musica m){
-        musicas.remove(m);
+    @Override
+    public void removeMusica(Musica m) {
+        this.musicas.remove(m);
+    }
+
+    @Override
+    public void imprimirLista() {
+        this.musicas.forEach(System.out::println);
+    }
+
+    public void percorrerPlayList(int tamanho){
+        int cont = 0;
+        ArrayList nova = new ArrayList<>();
+        Random random = new Random();
+        for(Musica m : musicas) {
+        }
+    }
+    public Musica encontrarMusicaGenero( String genero){
+        Musica g = null;
+        for(Musica m : musicas){
+            if(m.getGenero().equals(genero)){
+                g = (Musica) m;
+            }
+        }
+        return g;
     }
 
     public String getNome() {
