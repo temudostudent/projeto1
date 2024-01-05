73f823f0da32d238be666fd124d8437c2b1072ab
temudostudent <cesarneves@student.dei.uc.pt>
2024-01-05 11:36:54 +0000

    nova classe

--- ProjetoRockStar/src/Main.java
+++ ProjetoRockStar/src/Main.java
@@ -2,13 +2,15 @@ import java.io.IOException;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException{
 
         try {
             JanelaInicial run = new JanelaInicial();
         } catch (IOException e) {
+            e.printStackTrace();
             throw new RuntimeException(e);
         } catch (ClassNotFoundException e) {
+            e.printStackTrace();
             throw new RuntimeException(e);
         }
 
